c2206054180cd857ac4f0cda8d76bfe5a0e2f2e0
yakub <yakub3k+git@gmail.com>
2026-02-14 15:30:13 +0100

    Added - web app and database

---
+++ web-app/src/main/java/com/example/webapp/WebAppApplication.java
@@ -0,0 +1,22 @@
+package com.example.webapp;
+
+import com.example.webapp.data.repository.RoomRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(RoomRepository roomRepository) {
+        return args -> {
+            roomRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
